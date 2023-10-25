2a1e4305b2ec9fe1678e9ba67f0144a5e61fb79f
rafahuqe <arinahuqe23@gmail.com>
2023-10-25 07:45:30 +0600

    added brand cards

--- src/AllComponents/BrandsBody.jsx
+++ src/AllComponents/BrandsBody.jsx
@@ -20,9 +20,9 @@ const BrandsBody = ({ brands }) => {
       onClick={handleAdsBrand}
       className="max-w-6xl mx-auto  ">
         <h2></h2>
-      <div className="card h-[340px] border p-4 gap-3 m-10 shadow-xl hover:bg-red-400   hover:text-white">
+      <div className="card h-[350px] border p-4 gap-3 m-10 shadow-xl hover:bg-purple-400   hover:text-white">
         <figure>
-          <img src={photoURL} className="h-[240px] w-full" alt="Shoes" />
+          <img src={photoURL} className="h-[210px] w-full" alt="Shoes" />
         </figure>
         <div className="card-body">
           <h2 className="text-3xl font-bold text-center">{name}</h2>
